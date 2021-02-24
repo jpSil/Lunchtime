31d7a0adde273132f10aa57d20213c8ca010ba86
jpSil <jp.silander@gmail.com>
2021-02-24 16:33:09 +0200

    Added mock data to server.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,51 @@
+const { request } = require('express');
 const express = require('express');
 const server = express();
 const port = 5000;
 
-server.get('/', (request, response) => {
-    response.send("Hello world!");
+let recipes = [
+  recipe = {
+      "id": 1,
+      "name": "Ceasar Salad",
+      "description:": "A tasty chicken salad with ceasar dressing",
+      "ingredients": [
+          {
+              "item": "green salad",
+              "quantity": 200,
+              "unit": "gr"
+          },
+          {
+              "item": "tomato",
+              "quantity": 100,
+              "unit": "gr"
+          },
+          {
+              "item": "chicken",
+              "quantity": 250,
+              "unit": "gr"
+          }
+      ],
+      "instructions": [
+          {
+              "instruction": "Wash and rinse salad"
+          },
+          {
+              "instruction": "Slice and dice tomato"
+          },
+          {
+              "instruction": "Cut chicken and heat on pan"
+          }
+      ]
+    }
+]
+
+server.get('/recipes', (request, response) => {
+    response.send(recipes);
+})
+
+server.get('/recipes/:id', (request, response) => {
+    const result = recipes.filter((r) => r.id === parseInt(request.params.id));
+    response.send(result);
 })
 
 server.listen(port, () =>{
